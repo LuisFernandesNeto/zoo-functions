5f01311ef5f6b17d32068c278ac2516cb83f3fea
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-06-29 17:25:55 -0300

    Projeto, Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (...ids) => data.species.filter((id) => ids.includes(id.id));
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
