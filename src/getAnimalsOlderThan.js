0ab90a4ff294c8247478dfcc33fd5b5160712943
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-06-29 19:09:19 -0300

    Projeto, requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+const getAnimalsOlderThan = (animal, age) => {
+  const nome = data.species.find((specie) => specie.name === animal);
+  const idade = nome.residents.every((ano) => ano.age >= age);
+  return idade;
 }
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
