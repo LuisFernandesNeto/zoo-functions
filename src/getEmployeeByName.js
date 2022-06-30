32bab7a3d38594cdf279cbc2e5bfc73b1de584d6
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-06-30 14:29:49 -0300

    Requisito 3, mas o lint não ajuda

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => {
+  if (employeeName === undefined) {
+    return {};
+  }
+  const name = data.employees.find((employee) => {
+    if (employee.firstName === employeeName || employee.lastName === employeeName) {
+      return employee;
+    }
+  });
+  return name;
+};
+console.log(getEmployeeByName('Wilburn'));
 
 module.exports = getEmployeeByName;
