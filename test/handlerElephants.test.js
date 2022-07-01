6070ba766fddef2476b9e90412bd3cc05a1438a0
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-06-30 21:16:04 -0300

    Requisito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
     expect(handlerElephants('averageAge')).toBe(10.5);
   });
+  it('Para o argumento location deve retornar a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Para o argumento popularity deve retornar um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+  it('Para o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('availability')).toEqual(expect.not.arrayContaining(['Monday']));
+  });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
 });
