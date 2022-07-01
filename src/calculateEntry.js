0dc965285901762ecb0032390d87e72077256db5
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-07-01 17:56:31 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,32 +1,27 @@
 const data = require('../data/zoo_data');
 
-
-
 const countEntrants = (entrants) => entrants.reduce((acc, entrant) => {
   if (entrant.age < 18) {
     acc.child += 1;
   }
-  if (entrant.age > 18 && entrant.age < 50) {
+  if (entrant.age >= 18 && entrant.age < 50) {
     acc.adult += 1;
   }
   if (entrant.age >= 50) {
-   acc.senior += 1; 
+    acc.senior += 1;
   }
   return acc;
-}, {child: 0, adult: 0, senior: 0});
-
-  console.log(countEntrants([
-    { name:  'Lara Carvalho', age:  5 },
-    { name:  'Frederico Moreira', age:  5 },
-    { name:  'Pedro Henrique Carvalho', age:  5 },
-    { name:  'Maria Costa', age:  18 },
-    { name:  'Núbia Souza', age:  18 },
-    { name:  'Carlos Nogueira', age:  50 },
-  ]));
-
+}, { child: 0, adult: 0, senior: 0 });
 
 const calculateEntry = (entrants) => {
-  
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const primeiraFuncao = countEntrants(entrants);
+  const totalChildren = primeiraFuncao.child * 20.99;
+  const totalAdults = primeiraFuncao.adult * 49.99;
+  const totalSeniors = primeiraFuncao.senior * 24.99;
+  return totalChildren + totalAdults + totalSeniors;
 };
 
 module.exports = { calculateEntry, countEntrants };
