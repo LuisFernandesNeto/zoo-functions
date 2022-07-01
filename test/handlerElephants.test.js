869628df685ebfac762fec6a660415c8bcdc645c
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-06-30 21:00:29 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Para o argumento count deve retornar o número inteiro 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
+    expect(handlerElephants('names')).toEqual(expect.arrayContaining(['Jefferson']));
+  });
+  it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+});
