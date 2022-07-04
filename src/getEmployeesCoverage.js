61cb360c48dc0b9d7674d58f7f115ef6155c2e35
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-07-04 17:56:46 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees, species } = data;
+
+const arrEmployee = (names) => employees.filter((employee) => employee.firstName === names.name
+|| employee.lastName === names.name || employee.id === names.id);
+
+const objEmployee = (names) => (
+  arrEmployee(names).reduce((acc, curr) => {
+    acc.id = curr.id;
+    acc.fullName = `${curr.firstName} ${curr.lastName}`;
+    acc.species = curr.responsibleFor.map((animal) => (
+      species.find((specie) => specie.id === animal).name
+    ));
+    acc.locations = curr.responsibleFor.map((animal) => (
+      species.find((specie) => specie.id === animal).location));
+    return acc;
+  }, {})
+);
+
+const everyEmployee = () => employees.reduce((acc, curr) => acc.concat(objEmployee(curr)), []);
+
+const getEmployeesCoverage = (names) => {
+  if (!names) return everyEmployee();
+  if (!objEmployee(names).id) throw Error('Informações inválidas');
+  return objEmployee(names);
+};
 
 module.exports = getEmployeesCoverage;
